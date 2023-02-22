a789380f157efc5db6cd872e0898ea1b74553e29
D038721 <esther.schmitz@sap.com>
2023-02-22 14:09:46 +0100

    [supernova] initial with Alertmanager fetch
    # Conflicts:
    #	alerts/ui/README.md
    #	alerts/ui/__mocks__/client.js
    #	alerts/ui/__mocks__/fileMock.js
    #	alerts/ui/__mocks__/styleMock.js
    #	alerts/ui/babel.config.js
    #	alerts/ui/jest.config.js
    #	alerts/ui/package.json
    #	alerts/ui/public/index.html
    #	alerts/ui/setupTests.js
    #	alerts/ui/src/App.test.js
    #	alerts/ui/src/index.js
    #	alerts/ui/src/styles.scss
    #	alerts/ui/tailwind.config.js

--- alerts/ui/tailwind.config.js
+++ alerts/ui/tailwind.config.js
@@ -1,8 +1,3 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 // opacity helper to make custom colors work with opacity
 function withOpacity(variableName) {
   return ({ opacityVariable, opacityValue }) => {
@@ -18,12 +13,12 @@ function withOpacity(variableName) {
 
 module.exports = {
   presets: [
-    require("juno-ui-components/build/lib/tailwind.config"), // important, do not change
+    require('juno-ui-components/tailwind.config')  // important, do not change
   ],
-  prefix: "", // important, do not change
+  prefix: '', // important, do not change
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   corePlugins: {
-    preflight: false, // important, do not change
+    preflight: false,  // important, do not change
   },
   theme: {},
   plugins: [],
