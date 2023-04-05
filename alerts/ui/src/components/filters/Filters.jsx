0ebeabb2173bcfc9573c85b6bf8a5cc221be811d
D038721 <esther.schmitz@sap.com>
2023-04-05 18:11:57 +0200

    [supernova] rename all components from *.js to *.jsx

--- alerts/ui/src/components/filters/Filters.jsx
+++ alerts/ui/src/components/filters/Filters.jsx
@@ -1,27 +1,11 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React from "react"
-import { Stack } from "juno-ui-components"
 
-import FilterSelect from "./FilterSelect"
-import FilterPills from "./FilterPills"
+const Filters = ({}) => {
 
-const filtersStyles = `
-  bg-theme-background-lvl-1
-  py-2
-  px-4
-  my-px
-`
-
-const Filters = () => {
   return (
-    <Stack direction="vertical" gap="4" className={`filters ${filtersStyles}`}>
-      <FilterSelect />
-      <FilterPills />
-    </Stack>
+    <>
+      Filters
+    </>
   )
 }
 
--- alerts/ui/src/components/regions/RegionsList.jsx
+++ alerts/ui/src/components/regions/RegionsList.jsx
@@ -1,43 +1,29 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React from "react"
 
-import {
-  useAlertsIsLoading,
-  useAlertsRegionsFiltered,
-  useAlertsSeverityCountsPerRegion,
-} from "../../hooks/useAppStore"
+import useStore from "../../hooks/useStore"
 import Region from "./Region"
 
 const regionsClasses = `
   grid
   auto-rows-fr
-  grid-cols-[repeat(auto-fit,_minmax(21rem,_1fr))]
+  grid-cols-[repeat(auto-fit,_minmax(18.75rem,_1fr))]
   gap-3
   mb-8
 `
 
 const RegionsList = () => {
-  const isLoading = useAlertsIsLoading()
-  const severityCountsPerRegion = useAlertsSeverityCountsPerRegion()
-  const regions = useAlertsRegionsFiltered()
+  const isLoading = useStore((state) => state.alerts.isLoading)
+  const severityCountsPerRegion = useStore((state) => state.alerts.severityCountsPerRegion)
 
   return (
     <>
-      {!isLoading && regions && (
+      { !isLoading &&
         <div className={`regions ${regionsClasses}`}>
-          {regions.map((region) => (
-            <Region
-              key={`${region}`}
-              region={region}
-              severityCounts={severityCountsPerRegion[region]}
-            />
+          { Object.keys(severityCountsPerRegion).sort().map((region) => (
+            <Region key={region} region={region} severityCounts={severityCountsPerRegion[region]} />
           ))}
         </div>
-      )}
+      }
     </>
   )
 }
