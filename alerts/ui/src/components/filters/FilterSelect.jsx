66824ef8ee4dfc6da400137d3b8142555f6bd389
Esther Schmitz <edda@users.noreply.github.com>
2023-04-21 18:23:37 +0200

    [supernova] Filters (#324)
    * [supernova] add filters: initial working version with add/remove filter, update counts
    
    * [supernova] rework store to use atomic exports
    
    * [supernova] fix store imports. add missing store exports
    
    * [supernova] load all possible filter values for a label. Value selection via Select instead of TextInput
    
    * [supernova] filter possible values by those that are already active
    
    * [supernova] extract all possible regions from original item list so that regionslist always contains all regions even if that region doesn't have any alerts that match the current filter criteria
    
    * [supernova] extract FilterPills into own component
    
    * [supernova] add clear all filters functionality
    
    * [supernova] show -- instead of '0' when no alerts present for a severity
    
    * [supernova] fix initial setting of itemsFiltered (now in state action instead of in api hook). Name store actions for easier tracking in Redux devtools
    
    * [supernova] update version

--- alerts/ui/src/components/filters/FilterSelect.jsx
+++ alerts/ui/src/components/filters/FilterSelect.jsx
@@ -1,50 +1,41 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React, { useState } from "react"
 
 import {
   Button,
   InputGroup,
-  SelectOption,
   Select,
+  SelectOption,
+  SelectRow,
   Stack,
-  SearchInput,
 } from "juno-ui-components"
-import {
-  useFilterLabels,
-  useFilterLabelValues,
-  useFilterActions,
-  useActiveFilters,
-  useSearchTerm,
-} from "../../hooks/useAppStore"
+import { useFilterLabels, useFilterLabelValues, useFilterActions, useActiveFilters } from "../../hooks/useStore"
 import { humanizeString } from "../../lib/utils"
 
+
 const FilterSelect = () => {
-  const [filterLabel, setFilterLabel] = useState("")
-  const [filterValue, setFilterValue] = useState("")
+  const [filterLabel, setFilterLabel] = useState()
+  const [filterValue, setFilterValue] = useState()
   const [resetKey, setResetKey] = useState(Date.now())
 
-  const {
-    addActiveFilter,
-    loadFilterLabelValues,
-    clearActiveFilters,
-    setSearchTerm,
-  } = useFilterActions()
-  const filterLabels = useFilterLabels()
+  const { addActiveFilter, loadFilterLabelValues, clearActiveFilters } = useFilterActions()
+  const filterLabels = useFilterLabels() 
   const filterLabelValues = useFilterLabelValues()
   const activeFilters = useActiveFilters()
-  const searchTerm = useSearchTerm()
 
   const handleFilterAdd = (value) => {
+    
     if (filterLabel && (filterValue || value)) {
       // add active filter to store
-      addActiveFilter(filterLabel, filterValue || value)
+      addActiveFilter(filterLabel, (filterValue || value))
 
       // reset filterValue
-      setFilterValue("")
+      setFilterValue(undefined)
+      // force key change to reset the Select component to its initial state
+      // so that the placeholder is rendered again. This is a workaround to fix an open issue 
+      // in Radix UI. See: https://github.com/radix-ui/primitives/issues/1569
+      setResetKey(Date.now())
+
+      // TODO: remove filterValue from available value list
     } else {
       // TODO: show error -> please select filter/value
     }
@@ -63,58 +54,48 @@ const FilterSelect = () => {
     handleFilterAdd(value)
   }
 
-  const handleSearchChange = (value) => {
-    // debounce setSearchTerm to avoid unnecessary re-renders
-    const debouncedSearchTerm = setTimeout(() => {
-      setSearchTerm(value.target.value)
-    }, 500)
-
-    // clear timeout if we have a new value
-    return () => clearTimeout(debouncedSearchTerm)
-  }
-
   // const handleKeyDown = (event) => {
   //   if (event.key === "Enter") {
   //     handleFilterValueChange()
-  //   }
+  //   } 
   // }
 
   return (
     <Stack alignment="center" gap="8">
       <InputGroup>
-        <Select
+        <SelectRow
           name="filter"
           className="filter-label-select w-64 mb-0"
           label="Filter"
           value={filterLabel}
-          onChange={(val) => handleFilterLabelChange(val)}
+          onValueChange={(val) => handleFilterLabelChange(val)}
         >
-          {filterLabels?.map((filter) => (
+          {filterLabels?.map((filterLabel) => (
             <SelectOption
-              value={filter}
-              label={humanizeString(filter)}
-              key={filter}
+              value={filterLabel}
+              label={humanizeString(filterLabel)}
+              key={filterLabel}
             />
           ))}
-        </Select>
+        </SelectRow>
         <Select
           name="filterValue"
           value={filterValue}
-          onChange={(value) => handleFilterValueChange(value)}
+          onValueChange={(value) => handleFilterValueChange(value)}
           disabled={filterLabelValues[filterLabel] ? false : true}
           loading={filterLabelValues[filterLabel]?.isLoading}
-          className="filter-value-select w-96 bg-theme-background-lvl-0"
+          className="filter-value-select w-96 bg-theme-background-lvl-1"
           key={resetKey}
         >
-          {filterLabelValues[filterLabel]?.values
-            ?.filter(
-              (value) =>
-                // filter out already active values for this label
-                !activeFilters[filterLabel]?.includes(value)
-            )
-            .map((value) => (
-              <SelectOption value={value} key={value} />
-            ))}
+          { filterLabelValues[filterLabel]?.values?.filter((value) =>
+              // filter out already active values for this label
+              !activeFilters[filterLabel]?.includes(value)
+            ).map((value) =>
+            <SelectOption
+              value={value}
+              key={value}
+            />
+          )}
         </Select>
         <Button
           onClick={() => handleFilterAdd()}
@@ -122,21 +103,13 @@ const FilterSelect = () => {
           className="py-[0.3rem]"
         />
       </InputGroup>
-      {activeFilters && Object.keys(activeFilters).length > 0 && (
+      {activeFilters && Object.keys(activeFilters).length > 0 &&
         <Button
-          label="Clear all"
+          label="Clear all filters"
           onClick={() => clearActiveFilters()}
           variant="subdued"
         />
-      )}
-      <SearchInput
-        placeholder="search term or regular expression"
-        className="w-96 ml-auto"
-        value={searchTerm || ""}
-        onSearch={(value) => setSearchTerm(value)}
-        onClear={() => setSearchTerm(null)}
-        onChange={(value) => handleSearchChange(value)}
-      />
+      }
     </Stack>
   )
 }
