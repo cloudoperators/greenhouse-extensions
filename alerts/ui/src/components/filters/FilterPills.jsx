b959ab10f8de62d4b1e4300d45e460faeabb3110
Esther Schmitz <edda@users.noreply.github.com>
2023-04-21 18:23:37 +0200

    [supernova] Filters (#324)
    * [supernova] add filters: initial working version with add/remove filter, update counts
    
    * [supernova] rework store to use atomic exports
    
    * [supernova] fix store imports. add missing store exports
    
    * [supernova] load all possible filter values for a label. Value selection via Select instead of TextInput
    
    * [supernova] filter possible values by those that are already active
    
    * [supernova] extract all possible regions from original item list so that regionslist always contains all regions even if that region doesn't have any alerts that match the current filter criteria
    
    * [supernova] extract FilterPills into own component
    
    * [supernova] add clear all filters functionality
    
    * [supernova] show -- instead of '0' when no alerts present for a severity
    
    * [supernova] fix initial setting of itemsFiltered (now in state action instead of in api hook). Name store actions for easier tracking in Redux devtools
    
    * [supernova] update version

--- alerts/ui/src/components/filters/FilterPills.jsx
+++ alerts/ui/src/components/filters/FilterPills.jsx
@@ -1,31 +1,29 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React from "react"
 
 import { Pill, Stack } from "juno-ui-components"
-import { useActiveFilters, useFilterActions } from "../../hooks/useAppStore"
+import { useActiveFilters, useFilterActions } from "../../hooks/useStore"
 
 const FilterPills = () => {
+
   const activeFilters = useActiveFilters()
   const { removeActiveFilter } = useFilterActions()
 
   return (
-    <Stack gap="2" wrap={true}>
-      {Object.entries(activeFilters).map(([key, values]) => {
-        return values.map((value) => (
-          <Pill
-            pillKey={key}
-            pillValue={value}
-            closeable
-            onClose={() => removeActiveFilter(key, value)}
-            key={`${key}:${value}`}
-          />
-        ))
-      })}
-    </Stack>
+    <Stack gap="2">
+        { Object.entries(activeFilters).map(([key, values]) => {
+          return (
+            values.map((value) => 
+              <Pill 
+                pillKey={key}
+                pillValue={value}
+                closeable
+                onClose={() => removeActiveFilter(key, value)}
+                key={`${key}:${value}`}
+              />
+            )
+          )
+        })}
+      </Stack>
   )
 }
 
