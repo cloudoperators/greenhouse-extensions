2c8887ee072d0582d8c927674efedd1cd24178fe
D038721 <esther.schmitz@sap.com>
2023-06-16 18:43:08 +0200

    [supernova] AlertDetails view. Extract resuable alert info into components to use in Alert and AlertDetail. findAlertByFingerprint store action

--- alerts/ui/src/components/alerts/shared/AlertIcon.jsx
+++ alerts/ui/src/components/alerts/shared/AlertIcon.jsx
@@ -1,14 +1,12 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React, { forwardRef } from "react"
 
 import { Icon } from "juno-ui-components"
 
-const AlertIcon = ({ severity }, ref) => {
+
+const AlertIcon = ({severity}, ref) => {
+
   const iconColor = () => {
+
     switch (severity) {
       case "critical":
         return "text-theme-danger"
@@ -17,6 +15,7 @@ const AlertIcon = ({ severity }, ref) => {
       case "info":
         return "text-theme-info"
     }
+
   }
 
   return (
@@ -24,7 +23,11 @@ const AlertIcon = ({ severity }, ref) => {
       {severity === "critical" ? (
         <Icon ref={ref} icon="danger" color={iconColor()} />
       ) : severity?.match(/^(warning|info)$/) ? (
-        <Icon icon={severity} color={iconColor()} ref={ref} />
+        <Icon
+          icon={severity}
+          color={iconColor()}
+          ref={ref}
+        />
       ) : (
         <Icon ref={ref} icon="errorOutline" />
       )}
