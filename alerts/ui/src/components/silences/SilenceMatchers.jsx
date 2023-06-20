c9be30d25f60a0c1f794874a8086699f200a9d91
D063222 <a.reuschenbach.puncernau@sap.com>
2023-06-20 13:50:49 +0200

    [supernova] add posibility of add excluded matchers

--- alerts/ui/src/components/silences/SilenceMatchers.jsx
+++ alerts/ui/src/components/silences/SilenceMatchers.jsx
@@ -1,35 +1,14 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React, { useMemo } from "react"
 
 import { Pill, Stack } from "juno-ui-components"
 
 // Matchers can be clicked if the matcher is configurable
-const SilenceMatchers = ({ matchers, onClickCallback, closeable }) => {
+const SilenceMatchers = ({ matchers, onCloseCallback }) => {
   // sort matchers by exclude flag
   const sortedMatchers = useMemo(() => {
     if (!matchers || matchers?.length <= 0) return []
     return matchers.sort((a, b) => a.configurable - b.configurable)
   }, [matchers])
-
-  // closeable --> display x in the pill: is true if the matcher is configurable and just in case is excluded
-  // clickable --> is true if the matcher is configurable
-  // conditionally add click and close handlers only if the matcher is configurable (this is important,
-  // because the Pill component styles reacts on the presence of an onClick handler)
-  const clickProps = (matcher) => {
-    const props = {
-      onClick: () => matcher.configurable && onClickCallback(matcher),
-      onClose: () => onClickCallback(matcher),
-    }
-    if (matcher.configurable) {
-      return props
-    } else {
-      return {}
-    }
-  }
 
   return (
     <Stack gap="2" alignment="start" wrap={true}>
@@ -38,16 +17,16 @@ const SilenceMatchers = ({ matchers, onClickCallback, closeable }) => {
           return (
             <Pill
               className={matcher.configurable ? "opacity-100" : "opacity-70"}
-              closeable={matcher.configurable && closeable}
-              {...clickProps(matcher)}
-              key={matcher.name}
-              pillKey={matcher.name}
+              closeable={!!matcher.configurable}
+              onClose={() => onCloseCallback(matcher)}
+              key={matcher.key}
+              pillKey={matcher.key}
               pillValue={matcher.value}
             />
           )
         })
       ) : (
-        <span className="text-theme-light">No excluded matchers found</span>
+        <span className="opacity-70">No matchers found</span>
       )}
     </Stack>
   )
--- alerts/ui/src/components/silences/SilenceNew.jsx
+++ alerts/ui/src/components/silences/SilenceNew.jsx
@@ -1,39 +1,22 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import React, { useLayoutEffect, useMemo, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import {
   Modal,
   Button,
   Box,
   Form,
-  Textarea,
-  TextInput,
-  Select,
+  TextareaRow,
+  TextInputRow,
+  SelectRow,
   SelectOption,
-  Message,
-  FormRow,
 } from "juno-ui-components"
 import {
   useAuthData,
-  useSilencesExcludedLabels,
-  useGlobalsApiEndpoint,
-  useSilencesActions,
-  useAlertEnrichedLabels,
-} from "../../hooks/useAppStore"
+  useSilencesExcludedLabelsHash,
+  useFilterLabels,
+} from "../../hooks/useStore"
 import { post } from "../../api/client"
 import AlertDescription from "../alerts/shared/AlertDescription"
 import SilenceNewAdvanced from "./SilenceNewAdvanced"
-import { DateTime } from "luxon"
-import {
-  latestExpirationDate,
-  DEFAULT_DURATION_OPTIONS,
-  getSelectOptions,
-  setupMatchers,
-} from "./silenceHelpers"
-import { parseError } from "../../helpers"
 
 const validateForm = (values) => {
   const invalidItems = {}
@@ -55,126 +38,82 @@ const errorHelpText = (messages) => {
 
 const DEFAULT_FORM_VALUES = { duration: "2", comment: "" }
 
-const SilenceNew = ({ alert, size, variant }) => {
+const SilenceNew = ({ alert }) => {
   const authData = useAuthData()
-  const apiEndpoint = useGlobalsApiEndpoint()
-  const excludedLabels = useSilencesExcludedLabels()
-  const { addLocalItem, getMappingSilences } = useSilencesActions()
-  const enrichedLabels = useAlertEnrichedLabels()
-
+  const excludedLabelsHash = useSilencesExcludedLabelsHash()
+  const filterLabels = useFilterLabels()
   const [displayNewSilence, setDisplayNewSilence] = useState(false)
   const [formState, setFormState] = useState(DEFAULT_FORM_VALUES)
-  const [expirationDate, setExpirationDate] = useState(null)
   const [showValidation, setShowValidation] = useState({})
-  const [error, setError] = useState(null)
-  const [success, setSuccess] = useState(null)
+  const [matchers, setMatchers] = useState([])
 
-  // Initialize form state on modal open
-  // Removed alert from dependencies since we take an screenshot of the global state on opening the modal
-  // This is due to if the alert changes (e.g. the alert receives a new silenceBy) while the modal is open, the form state will be reset
-  useLayoutEffect(() => {
-    if (!displayNewSilence) return
-    // reset form state with default values
-    setFormState({
-      ...formState,
-      ...DEFAULT_FORM_VALUES,
-      createdBy: authData?.parsed?.fullName,
-      matchers: setupMatchers(alert?.labels, excludedLabels, enrichedLabels),
+  const name = useMemo(() => {
+    return authData?.parsed?.fullName
+  }, [authData])
+
+  // init the matchers
+  // use displayNewSilence to reset the matchers on open and close the same alert
+  useEffect(() => {
+    if (!alert?.labels || !excludedLabelsHash || !filterLabels) return
+
+    let items = []
+    // allow just labels which are configured
+    filterLabels.forEach((filterLabel) => {
+      const value = alert?.labels?.[filterLabel]
+      if (value) {
+        const matcher = {
+          key: filterLabel,
+          value: value,
+          excluded: false,
+          configurable: false,
+        }
+        // copy excluded label values to a different array
+        if (excludedLabelsHash[filterLabel]) {
+          items.push({ ...matcher, excluded: true, configurable: true })
+        } else {
+          items.push(matcher)
+        }
+      }
     })
-    // get the latest expiration date from the existing silences
-    // recalculate always on open modal due to the fact that the silences or local silences
-    // may change without change in the alert
-    setExpirationDate(latestExpirationDate(getMappingSilences(alert)))
-    // reset other states
-    setError(null)
-    setSuccess(null)
-    setShowValidation({})
-  }, [displayNewSilence])
+    setMatchers(items)
+  }, [alert, excludedLabelsHash, filterLabels, displayNewSilence])
 
-  // collect options for select dropdown with time (2 hours, 12 hours, 1 day, 3 days, 7 days) which exceeds the expiration date
-  // on open panel retrigger the calculation
-  const durationOptions = useMemo(() => {
-    const options = getSelectOptions(expirationDate)
-    if (options.firstNotCovered) {
-      // reset the first option which is not covered by the existing silence
-      setFormState({ ...formState, duration: options.firstNotCovered.value })
-    }
-    return options.items
-  }, [expirationDate])
+  const onCloseModal = () => {
+    // reset state
+    setDisplayNewSilence(false)
+    setFormState(DEFAULT_FORM_VALUES)
+  }
 
   const onSubmitForm = () => {
-    setError(null)
-    setSuccess(null)
     const formValidation = validateForm(formState)
     setShowValidation(formValidation)
     if (Object.keys(formValidation).length > 0) return
-    let newFormState = { ...formState }
-    // clean up attributes in matchers and remove excluded
-    if (newFormState.matchers?.length > 0) {
-      newFormState.matchers = newFormState.matchers
-        .filter((m) => !m.excluded)
-        .map(({ excluded, configurable, ...keepAttrs }) => keepAttrs)
-    }
-    // add extra attributes
-    const startsAt = new Date()
-    const endsAt = new Date()
-    endsAt.setHours(
-      endsAt.getHours() + Number.parseInt(newFormState.duration || 4)
-    )
-
-    const newSilence = {
-      ...newFormState,
-      status: { state: "processing" },
-      startsAt: startsAt.toISOString(),
-      endsAt: endsAt.toISOString(),
-    }
-
-    // submit silence
-    post(`${apiEndpoint}/silences`, {
-      body: JSON.stringify(newSilence),
-    })
-      .then((data) => {
-        setSuccess(data)
-        if (data?.silenceID) {
-          // add silence to local store
-          addLocalItem({
-            silence: newSilence,
-            id: data.silenceID,
-            alertFingerprint: alert.fingerprint,
-          })
-        }
-      })
-      .catch((error) => {
-        setError(parseError(error))
-      })
   }
 
   const onInputChanged = ({ key, value }) => {
-    if (!value) return
     setFormState({ ...formState, [key]: value })
   }
 
   const onMatchersChanged = (matcher) => {
-    const index = formState.matchers.findIndex(
-      (item) => item.name == matcher.name
-    )
-    let items = formState.matchers.slice()
+    const index = matchers.findIndex((item) => item.key == matcher.key)
+    let items = matchers.slice()
     // update item
     if (index >= 0) {
-      items[index] = { ...items[index], excluded: !matcher.excluded }
+      items[index] = {
+        key: matcher.key,
+        value: matcher.value,
+        excluded: !matcher.excluded,
+        configurable: matcher.configurable,
+      }
     }
-    setFormState({ ...formState, matchers: items })
+    setMatchers(items)
   }
 
   return (
     <>
       <Button
-        size={size}
-        variant={variant}
-        onClick={(e) => {
-          e.stopPropagation()
-          setDisplayNewSilence(!displayNewSilence)
-        }}
+        size="small"
+        onClick={() => setDisplayNewSilence(!displayNewSilence)}
       >
         Silence
       </Button>
@@ -183,92 +122,52 @@ const SilenceNew = ({ alert, size, variant }) => {
           title="New Silence for"
           size="large"
           open={true}
-          confirmButtonLabel={success ? null : "Save"}
-          onCancel={() => setDisplayNewSilence(false)}
-          onConfirm={success ? null : onSubmitForm}
+          confirmButtonLabel="Save"
+          onCancel={onCloseModal}
+          onConfirm={onSubmitForm}
         >
-          {error && <Message text={error} variant="danger" />}
-
-          {success && (
-            <Message className="mb-6" variant="success">
-              A silence object with id <b>{success?.silenceID}</b> was created
-              successfully. Please note that it may take up to 5 minutes for the
-              alert to show up as silenced.
-            </Message>
-          )}
-
-          {expirationDate && !success && (
-            <Message className="mb-6" variant="info">
-              There is already a silence for this alert that expires at{" "}
-              <b>
-                {DateTime.fromISO(expirationDate).toLocaleString(
-                  DateTime.DATETIME_SHORT
-                )}
-              </b>
-            </Message>
-          )}
-
-          <span className="text-lg font-bold">{alert?.labels?.alertname}</span>
+          <span className="text-lg">
+            <b>{alert?.labels?.alertname}</b>
+          </span>
 
           <Box className="mt-2">
             <AlertDescription description={alert.annotations?.description} />
           </Box>
 
-          {!success && (
-            <>
-              <SilenceNewAdvanced
-                matchers={formState.matchers}
-                onMatchersChanged={onMatchersChanged}
-              />
+          <SilenceNewAdvanced
+            matchers={matchers}
+            onMatchersChanged={onMatchersChanged}
+          />
 
-              <Form className="mt-6">
-                <FormRow>
-                  <TextInput
-                    required
-                    label="Silenced by"
-                    value={formState.createdBy}
-                    disabled
-                  />
-                </FormRow>
-                <FormRow>
-                  <Textarea
-                    className="h-20"
-                    label="Description"
-                    value={formState.comment}
-                    onChange={(e) =>
-                      onInputChanged({ key: "comment", value: e.target.value })
-                    }
-                    errortext={
-                      showValidation["comment"] &&
-                      errorHelpText(showValidation["comment"])
-                    }
-                    required
-                  />
-                </FormRow>
-                <FormRow>
-                  <Select
-                    required
-                    label="Duration"
-                    value={formState.duration}
-                    onChange={(value) =>
-                      onInputChanged({
-                        key: "duration",
-                        value,
-                      })
-                    }
-                  >
-                    {durationOptions?.map((option) => (
-                      <SelectOption
-                        key={option.value}
-                        label={option.label}
-                        value={option.value}
-                      />
-                    ))}
-                  </Select>
-                </FormRow>
-              </Form>
-            </>
-          )}
+          <Form className="mt-6">
+            <TextInputRow required label="Silenced by" value={name} disabled />
+            <TextareaRow
+              label="Description"
+              value={formState.comment}
+              onChange={(e) =>
+                onInputChanged({ key: "comment", value: e.target.value })
+              }
+              helptext={
+                showValidation["comment"] &&
+                errorHelpText(showValidation["comment"])
+              }
+              required
+            />
+            <SelectRow
+              required
+              label="Duration"
+              defaultValue={formState.duration}
+              onValueChange={(e) =>
+                onInputChanged({ key: "duration", value: e })
+              }
+            >
+              <SelectOption label="2 hours" value="2" />
+              <SelectOption label="12 hours" value="12" />
+              <SelectOption label="1 day" value="24" />
+              <SelectOption label="3 days" value="72" />
+              <SelectOption label="7 days" value="168" />
+            </SelectRow>
+          </Form>
         </Modal>
       )}
     </>
