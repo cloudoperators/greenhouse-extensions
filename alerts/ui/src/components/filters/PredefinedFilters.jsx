c3c254133aef5d43b6819fe04451306e9aaf0bef
D038721 <esther.schmitz@sap.com>
2023-06-23 16:55:36 +0200

    [supernova] add predefined filter functionality

--- alerts/ui/src/components/filters/PredefinedFilters.jsx
+++ alerts/ui/src/components/filters/PredefinedFilters.jsx
@@ -1,48 +1,26 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
+import React from "react"
 
-import React, { useState } from "react"
+import { SwitchRow } from "juno-ui-components"
+import { useFilterActions, usePredefinedFilters } from "../../hooks/useStore"
 
-import { Stack, TabNavigation, TabNavigationItem } from "juno-ui-components"
-import {
-  useActivePredefinedFilter,
-  useFilterActions,
-  usePredefinedFilters,
-} from "../../hooks/useAppStore"
-import SilenceScheduledWrapper from "../silences/SilenceScheduledWrapper"
 
 const PredefinedFilters = () => {
-  const { setActivePredefinedFilter } = useFilterActions()
+  const { togglePredefinedFilter } = useFilterActions()
   const predefinedFilters = usePredefinedFilters()
-  const activePredefinedFilter = useActivePredefinedFilter()
-
-  const [selectedItem, setSelectedItem] = useState(activePredefinedFilter)
-
-  const handleTabSelect = (item) => {
-    setSelectedItem(item)
-    setActivePredefinedFilter(item)
-  }
 
   return (
-    <Stack>
-      <TabNavigation
-        activeItem={selectedItem}
-        onActiveItemChange={handleTabSelect}
-      >
-        {predefinedFilters.map((filter) => (
-          <TabNavigationItem
-            key={filter.name}
-            value={filter.name}
-            label={filter.displayName}
-          />
-        ))}
-      </TabNavigation>
-      <div className="ml-auto">
-        <SilenceScheduledWrapper />
-      </div>
-    </Stack>
+    <div>
+      { predefinedFilters.map((filter) => (
+        <SwitchRow
+          label={filter.displayName}
+          name={filter.name}
+          on={filter.active}
+          onChange={() => togglePredefinedFilter(filter.name)}
+          key={filter.name}
+        />
+      ))}
+
+    </div>
   )
 }
 
