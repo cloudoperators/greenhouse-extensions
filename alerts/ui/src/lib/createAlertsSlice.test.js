795cb24a9bc8d9e0295064466b8f504cfe2af9f3
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2023-07-18 12:25:29 +0200

    [supernova]: added default enrichedLabels per configuration

--- alerts/ui/src/lib/createAlertsSlice.test.js
+++ alerts/ui/src/lib/createAlertsSlice.test.js
@@ -1,23 +1,17 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import * as React from "react"
 import { renderHook, act } from "@testing-library/react"
-import {
+import {  
   useAlertsActions,
   useAlertEnrichedLabels,
-  StoreProvider,
-  useAlertsItemsFiltered,
+  StoreProvider
 } from "../hooks/useAppStore"
 
+
 describe("createAlertsSlice", () => {
+
   describe("setEnrichedLabels", () => {
-    it("return status as default", () => {
-      const wrapper = ({ children }) => (
-        <StoreProvider>{children}</StoreProvider>
-      )
+    it("return empty array as default", () => {
+      const wrapper = ({ children }) => <StoreProvider>{children}</StoreProvider>
       const store = renderHook(
         () => ({
           actions: useAlertsActions(),
@@ -25,44 +19,26 @@ describe("createAlertsSlice", () => {
         }),
         { wrapper }
       )
+      expect(store.result.current.enrichedLabels).toEqual([])
+    })
+  
+    it("accepts and transforms to array of strings coma separated strings containing the labels to use", () => {
+      const wrapper = ({ children }) => <StoreProvider>{children}</StoreProvider>
+      const store = renderHook(
+        () => ({
+          actions: useAlertsActions(),
+          enrichedLabels: useAlertEnrichedLabels(),
+        }),
+        { wrapper }
+      )
+  
+      act(() => {
+        store.result.current.actions.setEnrichedLabels("status")
+      })
+  
       expect(store.result.current.enrichedLabels).toEqual(["status"])
     })
+
   })
 
-  describe("setFilteredItems", () => {
-    it("return empty array as default", () => {
-      const wrapper = ({ children }) => (
-        <StoreProvider>{children}</StoreProvider>
-      )
-      const store = renderHook(
-        () => ({
-          actions: useAlertsActions(),
-          itemsFiltered: useAlertsItemsFiltered(),
-        }),
-        { wrapper }
-      )
-      expect(store.result.current.itemsFiltered).toEqual([])
-    })
-
-    it("accepts and transforms to array of strings coma separated strings containing the labels to use", () => {
-      const wrapper = ({ children }) => (
-        <StoreProvider>{children}</StoreProvider>
-      )
-      const store = renderHook(
-        () => ({
-          actions: useAlertsActions(),
-          itemsFiltered: useAlertsItemsFiltered(),
-        }),
-        { wrapper }
-      )
-
-      let mock
-
-      act(() => {
-        store.result.current.actions.setFilteredItems(mock)
-      })
-
-      expect(store.result.current.enrichedLabels).toEqual(mock)
-    })
-  })
 })
