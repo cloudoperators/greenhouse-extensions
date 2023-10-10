3c7b603b76e88c5e4fa23b7367ace5a28189e540
Arturo Reuschenbach Puncernau <a.reuschenbach.puncernau@sap.com>
2023-10-10 11:51:09 +0200

    [supernova]: encapsulate asynchronous Jobs and Watchers in a separate Component

--- alerts/ui/src/components/AsyncWorker.jsx
+++ alerts/ui/src/components/AsyncWorker.jsx
@@ -1,17 +1,14 @@
-/*
- * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import React from "react"
 import useCommunication from "../hooks/useCommunication"
 import useAlertmanagerAPI from "../hooks/useAlertmanagerAPI"
+import useInitialFilters from "../hooks/useInitialFilters"
 import useUrlState from "../hooks/useUrlState"
 
 const AsyncWorker = ({ endpoint }) => {
   useCommunication()
   useAlertmanagerAPI(endpoint)
   useUrlState()
+  useInitialFilters()
   return null
 }
 
