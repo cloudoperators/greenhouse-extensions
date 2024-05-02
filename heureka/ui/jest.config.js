9599debba993ffc42b451ace30f741cce42ee643
Arturo Reuschenbach <reuschenbach@gmail.com>
2024-05-02 13:39:29 +0200

    [ui] tests fix (#89)
    * [heureka] tests fix
    
    * [supernova] fix tests

--- heureka/ui/jest.config.js
+++ heureka/ui/jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transformIgnorePatterns: [
-    "node_modules/(?!(juno-ui-components|url-state-router|communicator|oauth)/)",
+    "node_modules/(?!(juno-ui-components|url-state-router|communicator|oauth|messages-provider)/)",
   ],
   moduleNameMapper: {
     // Jest currently doesn't support resources with query parameters.
