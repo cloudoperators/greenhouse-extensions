d3b6e385d40713389e475887d814294d02b84e7b
TilmanHaupt <tilman.haupt@sap.com>
2024-06-12 18:15:22 +0200

    feat(alerts/ui): recreate modal

--- alerts/ui/src/components/silences/RecreateSilence.jsx
+++ alerts/ui/src/components/silences/RecreateSilence.jsx
@@ -1,101 +1,267 @@
-import React, { useState, useMemo } from "react"
+/*
+ * SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Greenhouse contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React, { useLayoutEffect, useMemo, useState } from "react"
 import {
-  Button,
-  FormSection,
   Modal,
+  Button,
+  Box,
   Form,
+  Textarea,
+  TextInput,
+  Select,
+  SelectOption,
+  Message,
   FormRow,
-  Pill,
-  Stack,
-  DateTimePicker,
 } from "juno-ui-components"
 import {
+  useAuthData,
+  useSilencesExcludedLabels,
   useGlobalsApiEndpoint,
   useSilencesActions,
+  useAlertEnrichedLabels,
 } from "../../hooks/useAppStore"
-import { useActions } from "messages-provider"
+import { post } from "../../api/client"
+import SilenceNewAdvanced from "./SilenceNewAdvanced"
+import { DateTime } from "luxon"
+import { latestExpirationDate, getSelectOptions } from "./silenceHelpers"
 import { parseError } from "../../helpers"
 
-import { del } from "../../api/client"
+const validateForm = (values) => {
+  const invalidItems = {}
+  if (values?.comment?.length <= 3) {
+    if (!invalidItems["comment"]) invalidItems["comment"] = []
+    invalidItems["comment"].push(`Description can't be blank`)
+  }
 
-const ExpireSilence = (props) => {
-  const { addMessage } = useActions()
+  return invalidItems
+}
+
+const errorHelpText = (messages) => {
+  return messages.map((msg, i) => (
+    <span key={i} className="block text-theme-danger ">
+      {msg}
+    </span>
+  ))
+}
+
+const DEFAULT_FORM_VALUES = { duration: "2", comment: "" }
+
+const RecreateSilence = (props) => {
   const silence = props.silence
-  const [confirmationDialog, setConfirmationDialog] = useState(false)
+  const authData = useAuthData()
   const apiEndpoint = useGlobalsApiEndpoint()
 
-  const defaultDate = useMemo(() => {
-    const date = new Date()
-    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
-  }, [])
+  const { addLocalItem, getMappingSilences } = useSilencesActions()
 
-  const onCreateSilence = () => {
-    setConfirmationDialog(false)
-    return
+  const [displayNewSilence, setDisplayNewSilence] = useState(false)
+  const [formState, setFormState] = useState(DEFAULT_FORM_VALUES)
+  const [expirationDate, setExpirationDate] = useState(null)
+  const [showValidation, setShowValidation] = useState({})
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
+
+  // Initialize form state on modal open
+  // Removed alert from dependencies since we take an screenshot of the global state on opening the modal
+  // This is due to if the alert changes (e.g. the alert receives a new silenceBy) while the modal is open, the form state will be reset
+  useLayoutEffect(() => {
+    if (!displayNewSilence) return
+
+    console.log(silence)
+    // reset form state with default values
+    setFormState({
+      ...formState,
+      ...DEFAULT_FORM_VALUES,
+      createdBy: authData?.parsed?.fullName,
+      matchers: silence.matchers,
+      comment: silence.comment,
+    })
+    // get the latest expiration date from the existing silences
+    // recalculate always on open modal due to the fact that the silences or local silences
+    // may change without change in the alert
+    setExpirationDate(latestExpirationDate())
+    // reset other states
+    setError(null)
+    setSuccess(null)
+    setShowValidation({})
+  }, [displayNewSilence])
+
+  // collect options for select dropdown with time (2 hours, 12 hours, 1 day, 3 days, 7 days) which exceeds the expiration date
+  // on open panel retrigger the calculation
+  const durationOptions = useMemo(() => {
+    const options = getSelectOptions(expirationDate)
+    if (options.firstNotCovered) {
+      // reset the first option which is not covered by the existing silence
+      setFormState({ ...formState, duration: options.firstNotCovered.value })
+    }
+    return options.items
+  }, [expirationDate])
+
+  const onSubmitForm = () => {
+    setError(null)
+    setSuccess(null)
+    const formValidation = validateForm(formState)
+    setShowValidation(formValidation)
+    if (Object.keys(formValidation).length > 0) return
+    let newFormState = { ...formState }
+    // clean up attributes in matchers and remove excluded
+    if (newFormState.matchers?.length > 0) {
+      newFormState.matchers = newFormState.matchers
+        .filter((m) => !m.excluded)
+        .map(({ excluded, configurable, ...keepAttrs }) => keepAttrs)
+    }
+    // add extra attributes
+    const startsAt = new Date()
+    const endsAt = new Date()
+    endsAt.setHours(
+      endsAt.getHours() + Number.parseInt(newFormState.duration || 4)
+    )
+
+    const newSilence = {
+      ...newFormState,
+      status: { state: "creating" },
+      startsAt: startsAt.toISOString(),
+      endsAt: endsAt.toISOString(),
+    }
+
+    // submit silence
+    post(`${apiEndpoint}/silences`, {
+      body: JSON.stringify(newSilence),
+    })
+      .then((data) => {
+        setSuccess(data)
+        if (data?.silenceID) {
+          // add silence to local store
+          addLocalItem({
+            silence: newSilence,
+            id: data.silenceID,
+            type: "local",
+          })
+        }
+      })
+      .catch((error) => {
+        setError(parseError(error))
+      })
+  }
+
+  const onInputChanged = ({ key, value }) => {
+    if (!value) return
+    setFormState({ ...formState, [key]: value })
+  }
+
+  const onMatchersChanged = (matcher) => {
+    const index = formState.matchers.findIndex(
+      (item) => item.name == matcher.name
+    )
+    let items = formState.matchers.slice()
+    // update item
+    if (index >= 0) {
+      items[index] = { ...items[index], excluded: !matcher.excluded }
+    }
+    setFormState({ ...formState, matchers: items })
   }
 
   return (
     <>
-      <Button onClick={() => setConfirmationDialog(true)}>Recreate</Button>
-      {confirmationDialog && (
+      <Button
+        onClick={(e) => {
+          e.stopPropagation()
+          setDisplayNewSilence(!displayNewSilence)
+        }}
+      >
+        Recreate
+      </Button>
+      {displayNewSilence && (
         <Modal
-          cancelButtonLabel="Cancel"
-          confirmButtonLabel="Create Silence"
-          onCancel={() => setConfirmationDialog(false)}
-          onConfirm={onCreateSilence}
+          title="New Silence for"
+          size="large"
           open={true}
-          title="Confirmation needed"
+          confirmButtonLabel={success ? null : "Save"}
+          onCancel={() => setDisplayNewSilence(false)}
+          onConfirm={success ? null : onSubmitForm}
         >
-          <Form>
-            <FormSection>
-              <FormRow>
-                <p>{silence.comment}</p>
-              </FormRow>
-              <FormRow>
-                <div className="grid gap-2 grid-cols-2">
-                  <DateTimePicker
-                    value={silence?.startsAt || defaultDate}
-                    dateFormat="Y-m-d H:i:S"
-                    label="Select a start date"
-                    enableTime
-                    time_24hr
-                    required
-                    errortext={silence}
-                    onChange={() => {}}
-                    enableSeconds
-                  />
-                  <DateTimePicker
-                    value={silence?.endsAt || defaultDate}
-                    dateFormat="Y-m-d H:i:S"
-                    label="Select a end date"
-                    enableTime
-                    time_24hr
-                    required
-                    errortext={silence}
-                    onChange={() => {}}
-                    enableSeconds
-                  />
-                </div>
-              </FormRow>
+          {error && <Message text={error} variant="danger" />}
 
-              <FormRow>
-                <Stack gap="2" wrap={true}>
-                  {Object.keys(silence.matchers).map((label, index) => (
-                    <Pill
-                      key={index}
-                      pillKey={silence?.matchers?.[label].name}
-                      pillKeyLabel={silence?.matchers?.[label].name}
-                      pillValue={silence?.matchers?.[label].value}
-                      pillValueLabel={silence?.matchers?.[label].value}
-                    />
-                  ))}
-                </Stack>
-              </FormRow>
-            </FormSection>
-          </Form>
+          {success && (
+            <Message className="mb-6" variant="success">
+              A silence object with id <b>{success?.silenceID}</b> was created
+              successfully. Please note that it may take up to 5 minutes for the
+              alert to show up as silenced.
+            </Message>
+          )}
+
+          {expirationDate && !success && (
+            <Message className="mb-6" variant="info">
+              There is already a silence for this alert that expires at{" "}
+              <b>
+                {DateTime.fromISO(expirationDate).toLocaleString(
+                  DateTime.DATETIME_SHORT
+                )}
+              </b>
+            </Message>
+          )}
+
+          {!success && (
+            <>
+              <SilenceNewAdvanced
+                matchers={formState.matchers}
+                onMatchersChanged={onMatchersChanged}
+              />
+
+              <Form className="mt-6">
+                <FormRow>
+                  <TextInput
+                    required
+                    label="Silenced by"
+                    value={formState.createdBy}
+                    disabled
+                  />
+                </FormRow>
+                <FormRow>
+                  <Textarea
+                    className="h-20"
+                    label="Description"
+                    value={formState.comment}
+                    onChange={(e) =>
+                      onInputChanged({ key: "comment", value: e.target.value })
+                    }
+                    errortext={
+                      showValidation["comment"] &&
+                      errorHelpText(showValidation["comment"])
+                    }
+                    required
+                  />
+                </FormRow>
+                <FormRow>
+                  <Select
+                    required
+                    label="Duration"
+                    value={formState.duration}
+                    onChange={(value) =>
+                      onInputChanged({
+                        key: "duration",
+                        value,
+                      })
+                    }
+                  >
+                    {durationOptions?.map((option) => (
+                      <SelectOption
+                        key={option.value}
+                        label={option.label}
+                        value={option.value}
+                      />
+                    ))}
+                  </Select>
+                </FormRow>
+              </Form>
+            </>
+          )}
         </Modal>
       )}
     </>
   )
 }
-export default ExpireSilence
+
+export default RecreateSilence
