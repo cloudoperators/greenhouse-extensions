38795b987f029bf225765ddbbdc570ad67fb1e44
d065895 <hoda.noori@sap.com>
2024-06-27 10:43:26 +0200

    feat(heureka): add filtering and search functionality for services tab

--- heureka/ui/src/components/filters/FilterPills.jsx
+++ heureka/ui/src/components/filters/FilterPills.jsx
@@ -4,12 +4,22 @@
  */
 
 import React from "react"
+<<<<<<< HEAD
 import { Pill, Stack } from "@cloudoperators/juno-ui-components"
 import { useActiveFilters, useActions } from "../StoreProvider"
 
 const FilterPills = () => {
   const activeFilters = useActiveFilters()
   const { removeActiveFilter } = useActions()
+=======
+
+import { Pill, Stack } from "juno-ui-components"
+import { useActiveFilters, useFilterActions } from "../../hooks/useAppStore"
+
+const FilterPills = () => {
+  const activeFilters = useActiveFilters()
+  const { removeActiveFilter } = useFilterActions()
+>>>>>>> 931e76db (feat(heureka): add filtering and search functionality for services tab)
 
   return (
     <Stack gap="2" wrap={true}>
@@ -24,6 +34,10 @@ const FilterPills = () => {
           />
         ))
       })}
+<<<<<<< HEAD
+=======
+      :
+>>>>>>> 931e76db (feat(heureka): add filtering and search functionality for services tab)
     </Stack>
   )
 }
