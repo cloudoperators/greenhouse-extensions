89d5fe4638856c56967229bb327928da6df910d1
d065895 <hoda.noori@sap.com>
2024-06-27 10:50:16 +0200

    adjust store actions and update package-lock.json

--- heureka/ui/src/components/filters/FilterPills.jsx
+++ heureka/ui/src/components/filters/FilterPills.jsx
@@ -4,22 +4,12 @@
  */
 
 import React from "react"
-<<<<<<< HEAD
 import { Pill, Stack } from "@cloudoperators/juno-ui-components"
 import { useActiveFilters, useActions } from "../StoreProvider"
 
 const FilterPills = () => {
   const activeFilters = useActiveFilters()
   const { removeActiveFilter } = useActions()
-=======
-
-import { Pill, Stack } from "juno-ui-components"
-import { useActiveFilters, useFilterActions } from "../../hooks/useAppStore"
-
-const FilterPills = () => {
-  const activeFilters = useActiveFilters()
-  const { removeActiveFilter } = useFilterActions()
->>>>>>> 931e76db (feat(heureka): add filtering and search functionality for services tab)
 
   return (
     <Stack gap="2" wrap={true}>
@@ -34,10 +24,6 @@ const FilterPills = () => {
           />
         ))
       })}
-<<<<<<< HEAD
-=======
-      :
->>>>>>> 931e76db (feat(heureka): add filtering and search functionality for services tab)
     </Stack>
   )
 }
