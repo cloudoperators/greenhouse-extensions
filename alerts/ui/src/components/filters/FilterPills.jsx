6ff61395472d376b7f064d6c0e7facbe8c030076
TilmanHaupt <tilman.haupt@sap.com>
2024-07-16 16:59:56 +0200

    feat(alerts/ui): new logic to deactivate filters

--- alerts/ui/src/components/filters/FilterPills.jsx
+++ alerts/ui/src/components/filters/FilterPills.jsx
@@ -6,30 +6,69 @@
 import React from "react"
 
 import { Pill, Stack } from "@cloudoperators/juno-ui-components"
-import {
-  useActiveFilters,
-  useFilterActions,
-  usePausedFilters,
-} from "../../hooks/useAppStore"
+import { useActiveFilters, useFilterActions } from "../../hooks/useAppStore"
 
 const FilterPills = () => {
   const activeFilters = useActiveFilters()
-  const pausedFilter = usePausedFilters()
-  const {
-    removeActiveFilter,
-    addPausedFilter,
-    addActiveFilter,
-    removePausedFilter,
-  } = useFilterActions()
+  const [filters, setFilters] = useState({})
+  const { removeActiveFilter } = useFilterActions()
+
+  // useEffect Hook zur Aktualisierung der Filter
+  useEffect(() => {
+    setFilters((prevFilters) => {
+      const newFilters = { ...prevFilters }
+
+      for (let key in newFilters) {
+        if (newFilters.hasOwnProperty(key)) {
+          if (activeFilters[key]) {
+            const activeSet = new Set(activeFilters[key])
+
+            // Behalte inaktive Elemente und aktualisiere aktive
+            newFilters[key] = newFilters[key].map((item) => {
+              if (activeSet.has(item.value)) {
+                return { ...item, active: true }
+              }
+              return item // Behalte inaktive Elemente unverändert
+            })
+
+            // Füge neue aktive Elemente hinzu
+            activeFilters[key].forEach((value) => {
+              if (!newFilters[key].some((item) => item.value === value)) {
+                newFilters[key].push({ value, active: true })
+              }
+            })
+          }
+        }
+      }
+
+      // Hinzufügen neuer Kategorien aus activeFilters
+      for (let key in activeFilters) {
+        if (!newFilters.hasOwnProperty(key)) {
+          newFilters[key] = activeFilters[key].map((value) => ({
+            value,
+            active: true,
+          }))
+        }
+      }
+
+      return newFilters
+    })
+    console.log(filters)
+  }, [activeFilters])
 
   const pauseFilter = (key, value) => {
-    removeActiveFilter(key, value)
-    addPausedFilter(key, value)
-  }
+    setFilters((prevFilters) => {
+      const newFilters = { ...prevFilters }
+      if (newFilters[key]) {
+        newFilters[key] = newFilters[key].map((item) =>
+          item.value === value ? { ...item, active: false } : item
+        )
+      }
+      return newFilters
+    })
 
-  const activateFilter = (key, value) => {
-    removePausedFilter(key, value)
-    addActiveFilter(key, value)
+    // Optional: Aktualisiere auch activeFilters
+    removeActiveFilter(key, value)
   }
 
   return (
