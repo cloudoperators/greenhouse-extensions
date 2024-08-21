79bcc925c97509d88376ddb068f4ab5a88059827
hodanoori <107242553+hodanoori@users.noreply.github.com>
2024-08-21 20:05:17 +0200

    feat(heureka): Remove expensive live search  & improve styling (#333)
    * feat(heureka): Increase debounce time for searching
    
    * change the debounce time to 2 seconds and update version
    
    * Give the min column width to counts columns in services and components view
    
    * comment out debounce search to activate it later along with required store state update

--- heureka/ui/src/components/filters/FilterSelect.jsx
+++ heureka/ui/src/components/filters/FilterSelect.jsx
@@ -50,13 +50,14 @@ const FilterSelect = ({ entityName, isLoading }) => {
     handleFilterAdd(value)
   }
 
-  const handleSearchChange = (value) => {
+  // TODO: The live search should be implemented after having store update mechanism in place
+  /*const handleSearchChange = (value) => {
     // Debounce search term to avoid unnecessary re-renders
     const debouncedSearchTerm = setTimeout(() => {
       setSearchTerm(entityName, value.target.value)
-    }, 500)
+    }, 500) 
     return () => clearTimeout(debouncedSearchTerm)
-  }
+  }*/
 
   return (
     <Stack alignment="center" gap="8">
@@ -105,9 +106,9 @@ const FilterSelect = ({ entityName, isLoading }) => {
           placeholder="Search term or regular expression"
           className="w-96 ml-auto"
           value={searchTerm || ""}
-          onSearch={(value) => setSearchTerm(value)}
+          onSearch={(value) => setSearchTerm(entityName, value)}
           onClear={() => setSearchTerm(null)}
-          onChange={(value) => handleSearchChange(value)}
+          // onChange={(value) => handleSearchChange(value)}
         />
       )}
     </Stack>
