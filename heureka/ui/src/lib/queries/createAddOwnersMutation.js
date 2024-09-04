a63e69decae10c3becf171e6d0ccbd2b4cdf46ea
d065895 <hoda.noori@sap.com>
2024-09-04 15:49:02 +0200

    update version

--- heureka/ui/src/lib/queries/createAddOwnersMutation.js
+++ heureka/ui/src/lib/queries/createAddOwnersMutation.js
@@ -3,34 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*import { gql } from "graphql-request"
-
-// A function that dynamically generates a GraphQL mutation for adding multiple owners to a service
-// As the graphql API offers no way to add multiple owners at once, we have to call addOwnerToService for each owner separately
-export default (serviceId, userIds) => {
-  const mutations = userIds.map(
-    (userId, index) => `
-    u${index}: addOwnerToService(serviceId: "${serviceId}", userId: "${userId}") {
-      id
-      name
-      owners {
-        edges {
-          node {
-            id
-            name
-          }
-        }
-      }
-    }
-  `
-  )
-
-  return gql`
-    mutation {
-      ${mutations.join("")}
-    }
-  `
-}*/
 import { gql } from "graphql-request"
 
 // A function that dynamically generates a GraphQL mutation for adding multiple owners to a service
@@ -51,8 +23,6 @@ export default (serviceId, userIds) => {
     .join(", ")}) {${mutations.join(" ")}}`
     .replace(/\s+/g, " ")
     .trim() // Remove newlines and excessive spaces
-    .replace(/^\s*\n\s*/, "")
-    .replace(/\n\s*$/, "")
 
   return {
     mutation: gql`
